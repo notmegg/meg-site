336cb93eff3fd62cebd2ad6efcb45a546869ac72
Meg <83547181+notmegg@users.noreply.github.com>
2023-07-20 02:37:44 +0530

    framer motion and smooth scroll

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,9 @@
+"use client";
 import { MdOutlineEmail } from "react-icons/md";
 import { AiOutlineLinkedin, AiOutlineGithub } from "react-icons/ai";
 import Image from "next/image";
 import meg_pic from "../public/stuff/meg_pic.jpg";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
@@ -10,21 +12,50 @@ const Header = () => {
       className="flex flex-col mt-20  xl:mt-28 xl:flex-row w-full h-full"
     >
       <div className=" w-full text-center px-10 xl:w-1/2 xl:text-left xl:pl-28 text-white">
-        <h3 className=" text-xl xl:text-2xl py-2">Hi,</h3>
-        <h1 className=" bg-gradient-to-r from-orange1 to-orange3 text-4xl xl:text-5xl py-2 font-extrabold text-transparent bg-clip-text overflow-hidden whitespace-nowrap animate-typing">
+        <motion.h3
+          initial={{ y: -10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.1, delay: 0.4 }}
+          className=" text-xl xl:text-2xl py-2"
+        >
+          Hi,
+        </motion.h3>
+        <motion.h1
+          initial={{ y: -10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.1, delay: 0.4 }}
+          className=" bg-gradient-to-r from-orange1 to-orange3 text-4xl xl:text-5xl py-2 font-extrabold text-transparent bg-clip-text"
+        >
           I'm Meghana
-        </h1>
-        <h3 className=" text-2xl xl:text-3xl py-2 text-orange3">Blah blah</h3>
-        <p className=" text-base xl:text-lg leading-8 text-gray-300 mt-2">
+        </motion.h1>
+        <motion.h3
+          initial={{ y: -10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.1, delay: 0.4 }}
+          className=" text-2xl xl:text-3xl py-2 text-orange3"
+        >
+          Blah blah
+        </motion.h3>
+        <motion.p
+          initial={{ y: -10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.1, delay: 0.5 }}
+          className=" text-base xl:text-lg leading-8 text-gray-300 mt-2"
+        >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
           ipsa, qui nostrum id provident recusandae quisquam? Aliquid
           perferendis labore, consequatur quam nihil praesentium aliquam. Illum
           dignissimos repellendus asperiores eum magni.
-        </p>
-        <div className=" text-2xl flex justify-center xl:justify-start gap-14 xl:pl-20 xl:gap-24 text-orange-200 mt-10">
+        </motion.p>
+        <motion.div
+          initial={{ y: -10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.1, delay: 0.6 }}
+          className=" text-2xl flex justify-center xl:justify-start gap-14 xl:pl-20 xl:gap-24 text-orange-200 mt-10"
+        >
           <a
             className=" w-12 h-9 rounded-full hover:text-orange2 hover:translate-y-2 transition-all duration-300"
-            href="meghanaps.contact@gmail.com"
+            href="mailto:meghanaps.contact@gmail.com"
             target="_blank"
           >
             <MdOutlineEmail />
@@ -43,15 +74,17 @@ const Header = () => {
           >
             <AiOutlineGithub />
           </a>
-        </div>
+        </motion.div>
       </div>
 
-      <div className=" xl:w-1/2 flex justify-center ">
+      <motion.div initial={{ y: -10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.1, delay: 0.7 }}className=" xl:w-1/2 flex justify-center ">
         <Image
           className=" h-80 w-80 rounded-2xl xl:rounded-full xl:h-80 xl:w-80 border-4 xl:border-8 border-orange-400 xl:ml-32 xl:mt-10 mt-16"
           src={meg_pic}
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,44 +1,119 @@
+"use client";
 import Link from "next/link";
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import React from "react";
+import { MdOutlineClose } from "react-icons/md";
+
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-slate-800">
       <div className="w-full h-full mx-auto py-1 flex items-center justify-between">
-        <div>
-          <h1 className="ml-10 text-lg text-orange-500">LOGO</h1>
-        </div>
+        <Link
+          href="/"
+          onClick={() => {
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            });
+          }}
+        >
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.25 }}
+          >
+            <h1 className="ml-10 text-lg text-orange-500">LOGO</h1>
+          </motion.div>
+        </Link>
+        
         <div className="hidden mdl:inline-flex items-center gap-7 mdl:mr-10">
           <ul className="flex gap-7 text-sm font-light">
             <Link
-              href="#header"
+              href="/"
+              onClick={() => {
+                window.scrollTo({
+                  top: 0,
+                  behavior: "smooth",
+                });
+              }}
               className="flex items-center gap-1 text-white hover:text-orange2 cursor-pointer duration-300 nav-link"
             >
-              <li>HOME</li>
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1 }}
+              >
+                HOME
+              </motion.li>
             </Link>
             <Link
-              href="#skills"
+              href="/#skills"
+              scroll={true}
+              onClick={(e) => {
+                e.preventDefault();
+                document
+                  .getElementById("skills")
+                  .scrollIntoView({ behavior: "smooth" });
+              }}
               className="flex items-center gap-1 text-white hover:text-orange2 cursor-pointer duration-300 nav-link"
             >
-              <li>SKILLS</li>
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.1 }}
+              >
+                SKILLS
+              </motion.li>
             </Link>
             <Link
-              href="#projects"
+              href="/#projects"
+              scroll={true}
+              onClick={(e) => {
+                e.preventDefault();
+                document
+                  .getElementById("projects")
+                  .scrollIntoView({ behavior: "smooth" });
+              }}
               className="flex items-center gap-1 text-white hover:text-orange2 cursor-pointer duration-300 nav-link"
             >
-              <li>PROJECTS</li>
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.2 }}
+              >
+                PROJECTS
+              </motion.li>
             </Link>
           </ul>
           <a href="" target="_blank">
-            <button className="px-4 py-2 rounded-md text-orange2 text-sm border border-orange2 hover:bg-orange3 hover:bg-opacity-10 duration-300 cursor-pointer">
+            <motion.button
+              initial={{ y: -10, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.1, delay: 0.3 }}
+              className="px-4 py-2 rounded-md text-orange2 text-sm border border-orange2 hover:bg-orange3 hover:bg-opacity-10 duration-300 cursor-pointer"
+            >
               RESUME
-            </button>
+            </motion.button>
           </a>
         </div>
         {/* Hamburger icon */}
-        <div className="w-6 h-5 flex flex-col justify-between items-center mdl:hidden text-4xl text-orange1 cursor-pointer overflow-hidden group mr-7">
+        <motion.div
+          onClick={handleNav}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.25 }}
+          className="w-6 h-5 flex flex-col justify-between items-center mdl:hidden text-4xl text-orange1 cursor-pointer overflow-hidden group mr-7"
+        >
           <span className="w-full h-[2px] bg-orange1 inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300"></span>
           <span className="w-full h-[2px] bg-orange1 inline-flex transform translate-x-3 group-hover:translate-x-0 transition-all ease-in-out duration-300"></span>
           <span className="w-full h-[2px] bg-orange1 inline-flex transform translate-x-1 group-hover:translate-x-3 transition-all ease-in-out duration-300"></span>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
